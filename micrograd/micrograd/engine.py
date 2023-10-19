c9feca4f86fe6516eaa36eeff1103cb90d9e7413
Christopher Milan <chris.m.milan@gmail.com>
2023-10-19 15:27:06 -0700

    add solutions for micrograd

--- micrograd/micrograd/engine.py
+++ micrograd/micrograd/engine.py
@@ -24,20 +24,21 @@ class Value:
 
   def __mul__(self, other: Value | float):
     other: Value = other if isinstance(other, Value) else Value(other)
-    out: Value = ... # TODO
+    out: Value = Value(self.data * other.data, (self, other), "*")
 
     def _backward():
-      pass # TODO
+      self.grad += other.data * out.grad
+      other.grad += self.data * out.grad
     out._backward = _backward
 
     return out
   
   def __pow__(self, other: float):
     assert isinstance(other, (int, float)), "only supports int/float powers"
-    out: Value = ... # TODO
+    out: Value = Value(self.data ** other, (self,), f"**{other}")
 
     def _backward():
-      pass # TODO
+      self.grad += (other * self.data**(other - 1)) * out.grad
     out._backward = _backward
 
     return out
@@ -45,30 +46,30 @@ class Value:
   # See https://en.wikipedia.org/wiki/Rectifier_(neural_networks)
   # Note that we define the derivative of ReLU at 0 to be 0
   def relu(self):
-    out: Value = ... # TODO
+    out: Value = Value(0 if self.data < 0 else self.data, (self,), "ReLU")
 
     def _backward():
-      pass # TODO
+      self.grad += (out.data > 0) * out.grad
     out._backward = _backward
 
     return out
   
   # returns e to the power self
   def exp(self):
-    out: Value = ... # TODO
+    out: Value = Value(math.e ** self.data, (self,), f"exp")
 
     def _backward():
-      pass # TODO
+      self.grad += (math.e ** self.data) * out.grad
     out._backward = _backward
 
     return out
   
   # returns the log (base e) of self
   def log(self):
-    out: Value = ... # TODO
+    out: Value = Value(math.log(self.data), (self,), f"log")
 
     def _backward():
-      pass # TODO
+      self.grad += (1.0 / self.data) * out.grad
     out._backward = _backward
 
     return out
@@ -77,7 +78,14 @@ class Value:
     # First, topo sort all children in the graph
     # See https://en.wikipedia.org/wiki/Topological_sorting
     topo = []
-    # TODO
+    visited = set()
+    def build_topo(v):
+      if v not in visited:
+        visited.add(v)
+        for child in v._prev:
+          build_topo(child)
+        topo.append(v)
+    build_topo(self)
 
     self.grad = 1 # ie. dy/dy = 1
     for v in reversed(topo): v._backward()
--- micrograd/micrograd/nn.py
+++ micrograd/micrograd/nn.py
@@ -14,13 +14,14 @@ class Module(ABC):
 # Single Neuron class
 class Neuron(Module):
   def __init__(self, num_inputs: float, non_linear=True):
-    self.weights: list[Value] = ... # TODO (remember to initialize with random.uniform(-1,1)!)
-    self.bias: Value = ... # TODO (initialize at 0)
+    self.weights: list[Value] = [Value(random.uniform(-1,1)) for _ in range(num_inputs)]
+    self.bias: Value = Value(0)
     self.non_linear: bool = non_linear
   
   # process input x to neuron, return activation
   def __call__(self, x: Value | float) -> Value:
-    pass # TODO (NB: Remember to use ReLU if non-linear!)
+    act = sum((wi*xi for wi,xi in zip(self.weights, x)), self.bias)
+    return act.relu() if self.non_linear else act
 
   # returns list of all parameters
   def parameters(self) -> list[Value]:
@@ -32,11 +33,12 @@ class Neuron(Module):
 # Layer of neurons
 class Layer(Module):
   def __init__(self, num_inputs: float, num_outputs: float, **kwargs):
-    self.neurons: list[Neuron] = ... # TODO
+    self.neurons: list[Neuron] = [Neuron(num_inputs, **kwargs) for _ in range(num_outputs)]
 
   # process input x to layer, return activations of each neuron (in a list)
   def __call__(self, x: list[Value | float]) -> list[Value]:
-    pass # TODO
+    out = [n(x) for n in self.neurons]
+    return out[0] if len(out) == 1 else out # NB: this squeezes for ease of use
 
   # returns list of all parameters
   def parameters(self) -> list[Value]:
@@ -48,11 +50,14 @@ class Layer(Module):
 # Multi-layer perceptron (Fully connected, feedforward neural network)
 class MLP(Module):
   def __init__(self, num_inputs: float, layer_dims: list[float]):
-    self.layers: list[Layer] = ... # TODO (remember that the final layer is linear!)
+    size = [num_inputs] + layer_dims
+    self.layers: list[Layer] = [Layer(size[i], size[i+1], non_linear=i!=len(layer_dims)-1) for i in range(len(layer_dims))]
   
   # process input x to entire MLP, return final layer activations
   def __call__(self, x: list[Value | float] | np.ndarray) -> list[Value]:
-    pass # TODO
+    for layer in self.layers:
+      x = layer(x)
+    return x
 
   def parameters(self) -> list[Value]:
     return [p for layer in self.layers for p in layer.parameters()]
